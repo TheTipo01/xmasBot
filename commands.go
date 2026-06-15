a619698848079e1a6b0308426c8625c92b333551
Manuel Raimo <thetipo01@protonmail.com>
2026-06-16 00:05:46 +0200

    fix(commands): new syntax for embed building

--- commands.go
+++ commands.go
@@ -29,31 +29,31 @@ var (
 				url := e.SlashCommandInteractionData().String("link")
 				if isValidURL(url) {
 					c := make(chan struct{})
-					go SendEmbedInteraction(discord.NewEmbedBuilder().SetTitle(botName).AddField("Downloading",
+					go SendEmbedInteraction(discord.NewEmbed().WithTitle(botName).AddField("Downloading",
 						"Please wait...", false).
-						SetColor(0x7289DA).Build(), e, c, nil)
+						WithColor(0x7289DA), e, c, nil)
 
 					err := downloadSong(url)
 
 					<-c
 					if err != nil {
-						ModifyInteractionAndDelete(discord.NewEmbedBuilder().SetTitle(botName).
+						ModifyInteractionAndDelete(discord.NewEmbed().WithTitle(botName).
 							AddField("Error", err.Error(), false).
-							SetColor(0x7289DA).Build(), e, time.Second*10)
+							WithColor(0x7289DA), e, time.Second*10)
 					} else {
-						ModifyInteractionAndDelete(discord.NewEmbedBuilder().SetTitle(botName).
+						ModifyInteractionAndDelete(discord.NewEmbed().WithTitle(botName).
 							AddField("Success", "Song added successfully!", false).
-							SetColor(0x7289DA).Build(), e, time.Second*5)
+							WithColor(0x7289DA), e, time.Second*5)
 					}
 				} else {
-					ModifyInteractionAndDelete(discord.NewEmbedBuilder().SetTitle(botName).
+					ModifyInteractionAndDelete(discord.NewEmbed().WithTitle(botName).
 						AddField("Error", "Not a valid URL!", false).
-						SetColor(0x7289DA).Build(), e, time.Second*10)
+						WithColor(0x7289DA), e, time.Second*10)
 				}
 			} else {
-				SendAndDeleteEmbedInteraction(discord.NewEmbedBuilder().SetTitle(botName).
+				SendAndDeleteEmbedInteraction(discord.NewEmbed().WithTitle(botName).
 					AddField("Error", e.Member().User.Username+" is not in the sudoers file. this incident will be reported", false).
-					SetColor(0x7289DA).Build(), e, time.Second*10, nil)
+					WithColor(0x7289DA), e, time.Second*10, nil)
 			}
 		},
 	}
